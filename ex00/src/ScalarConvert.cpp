b6d7a9516645cf0e123dba83e5a920cc98327e6a
Alex Gilles <agilles@c1r5p12.42lehavre.fr>
2025-06-10 16:18:24 +0200

    checkInput all test

--- ex00/src/ScalarConvert.cpp
+++ ex00/src/ScalarConvert.cpp
@@ -6,7 +6,7 @@
 /*   By: agilles <agilles@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/03 16:34:27 by agilles           #+#    #+#             */
-/*   Updated: 2025/06/06 16:07:36 by agilles          ###   ########.fr       */
+/*   Updated: 2025/06/10 16:17:24 by agilles          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -86,11 +86,39 @@ int	ScalarConvert::checkInput()
 {
 	if (this->getInput().compare("nan") == 0 || this->getInput().compare("+inf") == 0 ||
 			this->getInput().compare("-inf")  == 0 || this->getInput().compare("+inff") == 0 || this->getInput().compare("-inff") == 0)
-			return (NAN_INF);
+		return (NAN_INF);
 	else if (this->getInput().length() == 1 &&
 				(this->getInput()[0] == '+' || this->getInput()[0] == '-' ||
 				this->getInput()[0] == 'f' || this->getInput()[0] == '.'))
 		return (CHAR);
+	else if (this->getInput().find_first_of("+-") != this->getInput().find_last_of("+-"))
+		return (ERROR);
+	else if (this->getInput().find_first_not_of("+-0123456789") == std::string::npos)
+		return (INT);
+	else if (this->getInput().find_first_not_of("+-0123456789.") == std::string::npos)
+	{
+		if (this->getInput().find_first_of(".") != this->getInput().find_last_of(".") ||
+				isdigit(this->getInput()[this->getInput().find_first_of(".") + 1]) != 0 ||
+				this->getInput().find_first_of(".") == 0
+				)
+			return (ERROR);
+		return (DOUBLE);
+	}
+	else if (this->getInput().find_first_not_of("+-0123456789.f") == std::string::npos)
+	{
+		if (this->getInput().find_first_of(".") != this->getInput().find_last_of(".") ||
+				this->getInput().find_first_of("f") != this->getInput().find_last_of("f") ||
+				this->getInput().find_first_of("f") - this->getInput().find_first_of(".") == 1 ||
+				this->getInput().find_first_of(".") == 0 ||
+				this->getInput()[this->getInput().find_first_of("f") + 1] != '\0'
+				)
+			return (ERROR);
+		return (FLOAT);
+	}
+	else if ((this->getInput().length() == 1 && isalpha(this->getInput()[0])) ||
+				(this->getInput().length() == 1 && isprint(this->getInput()[0])))
+		return (CHAR);
+	return (ERROR);
 }
 
 void ScalarConvert::ConvertInput()
@@ -104,5 +132,17 @@ void ScalarConvert::ConvertInput()
 	int		types[] = {CHAR, INT, FLOAT, DOUBLE};
 
 	this->_type = checkInput();
+
+	if (this->_type == NAN_INF)
+		return ;
+
+	for (int i = 0; i++; i < 4)
+	{
+		if (types[i] == this->_type)
+		{
+			(this->*fromPTRS[i])();
+			break;
+		}
+	}
 }
 
